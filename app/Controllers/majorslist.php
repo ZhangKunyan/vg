a4e15f694e565e29940c9d7fbb8fadddce776b91
Y.🐝 <oncwnuJtdiiZZBylL0UXnvsEwX5s@git.weixin.qq.comgit>
2020-05-30 15:57:14 +0800

    111

--- app/Controllers/majorslist.php
+++ app/Controllers/majorslist.php
@@ -7,21 +7,55 @@ class majorslist extends BaseController {
 		$this->db = \Config\Database::connect();
 	}
 
+//获取大类小类
 	public function getmajorsList() {
-		$sql = "select * from tb_majors";
-		$query = $this->db->query($sql);
-		$res = array();
-		//查大学列表
-		foreach ($query->getResultArray() as $row) {
+		// $sql = "select * from tb_majors";
+		// $query = $this->db->query($sql);
+		// $res = array();
+		// //查大学列表
+		// foreach ($query->getResultArray() as $row) {
 
-			$tmp = array();
-			$tmp["items.id"] = $row["majorid"];
-			$tmp["items.name"] = $row["name"];
-			$tmp["nameclass"] = $row["kind"];
+		// 	$tmp = array();
+		// 	$tmp["items.id"] = $row["majorid"];
+		// 	$tmp["items.name"] = $row["name"];
+		// 	$tmp["nameclass"] = $row["kind"];
 
-			$res[] = $tmp;
+		// 	$res[] = $tmp;
+		// }
+
+		// echo json_encode($res, true);
+
+		$majorslist = array();
+
+		$sql1 = "SELECT DISTINCT class FROM `tb_majors` WHERE 1";
+		$query1 = $this->db->query($sql1);
+
+		foreach ($query1->getResultArray() as $row) {
+			$name1 = $row["class"];
+			$item1 = array(
+				"name" => $name1,
+				"level" => 0,
+				"items" => array(),
+			);
+
+			$sql2 = "SELECT DISTINCT  classkid FROM `tb_majors` WHERE class = ? ";
+
+			$query2 = $this->db->query($sql2, $name1);
+
+			foreach ($query2->getResultArray() as $row) {
+				$name2 = $row["classkid"];
+				$item2 = array(
+					"name" => $name2,
+					"level" => 1,
+				);
+
+				$item1["items"][] = $item2;
+
+			}
+
+			$majorslist[] = $item1;
 		}
-
-		echo json_encode($res, true);
+		echo json_encode($majorslist, true);
 	}
+
 }
