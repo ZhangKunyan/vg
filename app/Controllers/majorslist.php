00b9860d5bd835325022306e7e9fb54e35ff3960
Y.🐝 <oncwnuJtdiiZZBylL0UXnvsEwX5s@git.weixin.qq.comgit>
2020-05-30 21:44:59 +0800

    1

--- app/Controllers/majorslist.php
+++ app/Controllers/majorslist.php
@@ -27,23 +27,23 @@ class majorslist extends BaseController {
 
 		$majorslist = array();
 
-		$sql1 = "SELECT DISTINCT class FROM `tb_majors` WHERE 1";
+		$sql1 = "SELECT DISTINCT major_class FROM `tb_majors` WHERE 1";
 		$query1 = $this->db->query($sql1);
 
 		foreach ($query1->getResultArray() as $row) {
-			$name1 = $row["class"];
+			$name1 = $row["major_class"];
 			$item1 = array(
 				"name" => $name1,
 				"level" => 0,
 				"items" => array(),
 			);
 
-			$sql2 = "SELECT DISTINCT  classkid FROM `tb_majors` WHERE class = ? ";
+			$sql2 = "SELECT DISTINCT  major_classkid FROM `tb_majors` WHERE major_class = ? ";
 
 			$query2 = $this->db->query($sql2, $name1);
 
 			foreach ($query2->getResultArray() as $row) {
-				$name2 = $row["classkid"];
+				$name2 = $row["major_classkid"];
 				$item2 = array(
 					"name" => $name2,
 					"level" => 1,
